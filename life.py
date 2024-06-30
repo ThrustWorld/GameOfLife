21d347fc4c3249c8f4edc777170cb44b21230e52
Emanuele Nepa <nepaemanuele@outlook.it>
2024-06-30 15:47:47 +0200

    - Housekeeping of the code inside functions - Add start() and updated() as main functions - Add clear button's logic - Add grid's logic based on a list of cells(0->dead 1->alive)

--- life.py
+++ life.py
@@ -1,16 +1,23 @@
 from ezgraphics import GraphicsWindow
 
-window = GraphicsWindow()
-window.setTitle("The game of Life")
+# global variables
+MAX_WIDTH = 400
+MAX_HEIGHT = 400
+CELL_SIZE = 25
+BUTTON_SIZE = 100
 
+# Set up window and canvas
+window = GraphicsWindow(MAX_WIDTH, MAX_HEIGHT)
 canvas = window.canvas()
 
+# Button events
 def step():
     #TODO
     return
 def clear():
-    #TODO
-    return
+    canvas.setFill(255,255,255)
+    draw_grid(MAX_WIDTH,MAX_HEIGHT-100)
+    init_cells(cells)
 def glider():
     #TODO
     return
@@ -18,56 +25,69 @@ def quit():
     return True
 
 
-def draw_grid(width,height):
-    cell_size = 25
-    cells_for_row = width // cell_size
-    cells_for_col = height // cell_size
-    
-    cells_list = []
+def init_buttons():
+    # 1.Step
+    canvas.drawRect(0,MAX_HEIGHT-100,BUTTON_SIZE,BUTTON_SIZE)
+    canvas.drawText(35,335,"STEP")
+    # 2.Clear
+    canvas.drawRect(MAX_WIDTH-300,MAX_HEIGHT-100,BUTTON_SIZE,BUTTON_SIZE)
+    canvas.drawText(130,335,"CLEAR")
+    # 3.Glider
+    canvas.drawRect(MAX_WIDTH-200,MAX_HEIGHT-100,BUTTON_SIZE,BUTTON_SIZE)
+    canvas.drawText(230,335,"GLIDER")
+    # 4.Quit
+    canvas.drawRect(MAX_WIDTH-100,MAX_HEIGHT-100,BUTTON_SIZE,BUTTON_SIZE)
+    canvas.drawText(335,335,"QUIT")
 
-    # list of cells
+
+def init_cells(cells_list):
+    cells_for_row = (MAX_HEIGHT-100) // CELL_SIZE
+    cells_for_col = MAX_WIDTH // CELL_SIZE
+
     for i in range(cells_for_col):
         for j in range(cells_for_row):
-            cells_list.append(0)
+            cells_list.append(0) # 0 = DEAD
 
 
-    # grid
-    canvas.setOutline(255,0,0)
+def draw_grid(width,height):
+    cells_for_row = height // CELL_SIZE
+    cells_for_col = width // CELL_SIZE
+
+    canvas.setOutline(255,0,0) # red
     for i in range(cells_for_row):
         for j in range(cells_for_col):   
-             canvas.drawRect(i * cell_size,j * cell_size, cell_size, cell_size)
+             canvas.drawRect(j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE)
 
 
-# Buttons
-# 1.Step
-canvas.drawRect(0,300,100,100)
-canvas.drawText(35,335,"STEP")
-# 2.Clear
-canvas.drawRect(100,300,100,100)
-canvas.drawText(130,335,"CLEAR")
-# 3.Glider
-canvas.drawRect(200,300,100,100)
-canvas.drawText(230,335,"GLIDER")
-# 4.Quit
-canvas.drawRect(300,300,100,100)
-canvas.drawText(335,335,"QUIT")
-
-draw_grid(400,300)
-
-done = False
-while not done:
-    mouse_xy = window.getMouse() # mouse coordinates for x and y when left mouse button is pressed
-
-    # Button events
+cells = []
+def start():
+    window.setTitle("The game of Life")
+    # Buttons
+    init_buttons()
     
-    if mouse_xy[0] > 300 and mouse_xy[1] > 300: # Quit event
-        done = quit()
-    elif mouse_xy[0] > 200 and mouse_xy[1] > 300: # Glider event
-        #glider()
-        print("Glider event!")
-    elif mouse_xy[0] > 100 and mouse_xy[1] > 300: # Clear event
-        #clear()
-        print("Clear event!")
-    elif mouse_xy[0] > 0 and mouse_xy[1] > 300: # Step event
-        #step()
-        print("Step event!")
+    # List of cells
+    init_cells(cells)
+    
+    # 2d grid
+    draw_grid(MAX_WIDTH,MAX_HEIGHT-100)
+
+def update():
+    done = False
+    while not done:
+        mouse_xy = window.getMouse() # mouse coordinates for x and y when left mouse button is pressed
+
+        # Button events
+        if mouse_xy[0] > MAX_WIDTH-100 and mouse_xy[1] > MAX_HEIGHT-100: # Quit event
+            done = quit()
+        elif mouse_xy[0] > MAX_WIDTH-200 and mouse_xy[1] > MAX_HEIGHT-100: # Glider event
+            #glider()
+            print("Glider event!")
+        elif mouse_xy[0] > MAX_WIDTH-300 and mouse_xy[1] > MAX_HEIGHT-100: # Clear event
+            clear()
+            print("Clear event!")
+        elif mouse_xy[0] > 0 and mouse_xy[1] > MAX_HEIGHT-100: # Step event
+            #step()
+            print("Step event!")
+
+start()
+update()
